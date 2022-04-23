b616cf2cd1d1ad268f5172a4368e698d8e24bd02
Adam <me@adammartincote.com>
2022-04-23 15:19:59 -0400

    Initial commit

---
+++ digitalocean-kubernetes/main.tf
@@ -0,0 +1,25 @@
+resource "digitalocean_kubernetes_cluster" "lab-kubernetes-cluster" {
+  name    = "django-kubernetes-tor1-1650372428244"
+  region  = "TOR1"
+  version = "1.22.8-do.1"
+
+  node_pool {
+    name       = "django-kubernetes-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
+resource "digitalocean_database_db" "django-database" {
+  cluster_id = digitalocean_database_cluster.lab-database-cluster.id
+  name       = "defaultdb"
+}
+
+resource "digitalocean_database_cluster" "lab-database-cluster" {
+  name       = "django-kubernetes"
+  engine     = "pg"
+  version    = "13"
+  size       = "db-s-1vcpu-1gb"
+  region     = "TOR1"
+  node_count = 1
+}
